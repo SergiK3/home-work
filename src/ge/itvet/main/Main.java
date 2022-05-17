43fe76af9f08145d302dd41364df8892b272bf57
SergiK3 <sergikotrikadze@itvet.ge>
2022-05-17 15:14:01 +0400

    Insertion sort

--- src/ge/itvet/main/Main.java
+++ src/ge/itvet/main/Main.java
@@ -17,7 +17,7 @@ public class Main {
         for (int j = 1; j < A.length; j++) {
             key = A[j];
             i = j - 1;
-            while (i >= 0 && A[i] > key) { // wignshi mkacri metoba ewera
+            while (i >= 0 && A[i] > key) {
                 A[i + 1] = A[i];
                 i = i - 1;
                 A[i + 1] = key;
