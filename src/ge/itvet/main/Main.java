a380a0fed5a06fefa22bd54521be9d31cf19dd97
SergiK3 <sergikotrikadze@itvet.ge>
2022-05-17 14:50:23 +0400

    Insertion sort

--- src/ge/itvet/main/Main.java
+++ src/ge/itvet/main/Main.java
@@ -1,29 +1,10 @@
 package ge.itvet.main;
 
-import ge.itvet.security.*;
 
-import java.util.UUID;
 
 public class Main {
-    public static void main(String[] args) throws ThereCantBeNegativeAmountOfPeopleException {
+    public static void main(String[] args)  {
 
-
-        Hospital amteli = new Hospital(new Gate[]{new Gate(), new Gate()}, "Amtel-Hospital");
-
-        amteli.getGateByIndex(0).enter();
-        amteli.getGateByIndex(0).enter();
-        amteli.getGateByIndex(0).enter();
-
-
-        System.out.println("There are - " + amteli.retrieveAmountOfPeopleInTheBuilding() + " - People in the hospital");
-
-        amteli.getGateByIndex(1).leave();
-        amteli.getGateByIndex(1).leave();
-
-        System.out.println("There are - " + amteli.retrieveAmountOfPeopleInTheBuilding() + " - People in the hospital");
-
-        System.out.println(amteli.getType().geoName);
-        System.out.println(amteli.getType().geoName);
 
     }
 
--- src/ge/itvet/security/ThereCantBeNegativeAmountOfPeopleException.java
+++
@@ -1,9 +0,0 @@
-package ge.itvet.security;
-
-public class ThereCantBeNegativeAmountOfPeopleException extends Exception {
-
-    public ThereCantBeNegativeAmountOfPeopleException(String message) {
-        super(message);
-    }
-
-}
