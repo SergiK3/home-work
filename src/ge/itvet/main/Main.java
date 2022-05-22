4170ddb71d6cf839fc0cd369fbda68fdf9305ce7
SergiK3 <sergikotrikadze@itvet.ge>
2022-05-22 14:23:43 +0400

    week 7

--- src/ge/itvet/main/Main.java
+++ src/ge/itvet/main/Main.java
@@ -1,29 +1,12 @@
 package ge.itvet.main;
 
 
-import java.util.Arrays;
+
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("week 7");
 
-        int[] array = {5,-55,99,6,0};
-        insertionSort(array);
-        System.out.println(Arrays.toString(array));
-    }
-
-    public static void insertionSort(int[] A) {
-        int key = 0;
-        int i = 0;
-        for (int j = 1; j < A.length; j++) {
-            key = A[j];
-            i = j - 1;
-            while (i >= 0 && A[i] > key) {
-                A[i + 1] = A[i];
-                i = i - 1;
-                A[i + 1] = key;
-            }
-
-        }
     }
 
 
