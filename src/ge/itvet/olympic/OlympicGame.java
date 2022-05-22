0e71c2e23f659fdb018a2b1a8cf11405066b8b7e
SergiK3 <sergikotrikadze@itvet.ge>
2022-05-22 16:25:24 +0400

    not all apparently

--- src/ge/itvet/olympic/OlympicGame.java
+++ src/ge/itvet/olympic/OlympicGame.java
@@ -1,6 +1,6 @@
 package ge.itvet.olympic;
 
-enum OlympicGame {
+public enum OlympicGame {
     BASKETBALL_3X3(OlympicGameSeason.SUMMER), ARCHERY(OlympicGameSeason.SUMMER), ARTISTIC_GYMNASTICS(OlympicGameSeason.SUMMER), ARTISTIC_SWIMMING(OlympicGameSeason.SUMMER), ATHLETICS(OlympicGameSeason.SUMMER), BADMINTON(OlympicGameSeason.SUMMER), BASEBALL_SOFTBALL(OlympicGameSeason.SUMMER),
     BASKETBALL(OlympicGameSeason.SUMMER), BEACH_VOLLEYBALL(OlympicGameSeason.SUMMER), BMX(OlympicGameSeason.SUMMER), BOXING(OlympicGameSeason.SUMMER), CANOE(OlympicGameSeason.SUMMER), DIVING(OlympicGameSeason.SUMMER), EQUESTRIAN(OlympicGameSeason.SUMMER), FENCING(OlympicGameSeason.SUMMER), FOOTBALL(OlympicGameSeason.SUMMER), GOLF(OlympicGameSeason.SUMMER), HANDBALL(OlympicGameSeason.SUMMER),
     HOCKEY(OlympicGameSeason.SUMMER), JUDO(OlympicGameSeason.SUMMER), KARATE(OlympicGameSeason.SUMMER), MARATHON_SWIMMING(OlympicGameSeason.SUMMER), MODERN_PENTATHLON(OlympicGameSeason.SUMMER), MOUNTAIN_BIKE(OlympicGameSeason.SUMMER), RHYTHMIC_GYMNASTICS(OlympicGameSeason.SUMMER),
