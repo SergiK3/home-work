072258ddc4e5bb764871e5e3658880c82bb6bb85
SergiK3 <sergikotrikadze@itvet.ge>
2022-05-22 14:57:32 +0400

    week 7

--- src/ge/itvet/exception/OlympicGameFutureException.java
+++ src/ge/itvet/exception/OlympicGameFutureException.java
@@ -3,6 +3,7 @@ package ge.itvet.exception;
 public class OlympicGameFutureException extends OlympicGameYearException {
 //დაუწერეთ კონსტრუქტორი, რომელსაც პარამეტრად გადმოეცემა წელი, და დააგენერირებს მესიჯს: მომავლის თარიღი:
 // ჯერ ოლიპიური თამაში ${პარამეტრად გადმოცემული წელი} წელს არ ჩატარებულა.
+
     public OlympicGameFutureException(int year) {
         super(year);
         System.err.println(String.format("ჯერ ოლიპიური თამაში %d წელს არ ჩატარებულა.",year));
