9c4b6b405d599d93a4cd664c8516d129835ac9df
SergiK3 <sergikotrikadze@itvet.ge>
2022-06-01 17:58:52 +0400

    week 8 in progress,reader done

--- src/ge/itvet/main/Main.java
+++ src/ge/itvet/main/Main.java
@@ -3,11 +3,31 @@ package ge.itvet.main;
 
 import ge.itvet.gates.EntranceAction;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("src\\in.csv"));
 
 
 
+
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
