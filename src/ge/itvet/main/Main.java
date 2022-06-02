53eb3f4c7fd9fbff53f36258bc94717b8e1309c4
sergi <sergikotrikadze@itvet.ge>
2022-06-02 19:49:52 +0400

    Engine class done not vehicle

--- src/ge/itvet/main/Main.java
+++ src/ge/itvet/main/Main.java
@@ -5,7 +5,6 @@ import ge.itvet.pti.EngineType;
 public class Main {
     public static void main(String[] args) {
 
-        EngineType hybrid = EngineType.ELECTRIC;
-        System.out.println(hybrid.hasEmission());
+
     }
 }
