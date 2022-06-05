b5ef58aa6316f472e872b2a5dc14a3c289a053c4
SergiK3 <sergikotrikadze@itvet.ge>
2022-06-05 20:51:03 +0400

    done

--- src/ge/itvet/main/Main.java
+++ src/ge/itvet/main/Main.java
@@ -6,11 +6,20 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(uniqueElements(1, 2, 3, 4, 2, 5, 1, 6, 7));
+        Scanner scanner = new Scanner(System.in);
+
+        int[] array = new int[Integer.parseInt(scanner.nextLine())];
+        String[] array1 = scanner.nextLine().split(" ");
+
+        for (int i = 0; i < array1.length; i++) {
+            array[i] = Integer.parseInt(array1[i]);
+        }
+        System.out.println(uniqueElements(array));
+        scanner.close();
 
     }
 
-    public static int uniqueElements(int... arr) {
+    public static int uniqueElements(int[] arr) {
         int notUniqueElement = 0;
         for (int j = 0; j < arr.length; j++) {
             for (int i = 0; i < arr.length; i++) {
